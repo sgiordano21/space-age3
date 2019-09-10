ca30ca5a6887dd2ffd8a8311ac0653870b893d54
sgiordano <sofia.s.giordano@gmail.com>
2019-09-09 21:30:10 -0700

    successfully run test on life expectancy

--- spec/space-age3-spec.js
+++ spec/space-age3-spec.js
@@ -1,49 +1,50 @@
-import { Person } from './../src/scripts.js';
+import {
+  Person
+} from './../src/scripts.js';
 
-describe ('Person', function() {
- it('should only accept numerical input', function() {
-   let exampleAge = new Person (5);
-   expect(exampleAge.checkDigits()).toBe(5);
- });
+describe('Person', function() {
+  it('should only accept numerical input', function() {
+    let exampleAge = new Person(5);
+    expect(exampleAge.checkDigits()).toBe(5);
+  });
 });
 
 
-describe ('Person', function(){
-  it('should return how old a person is in mercurian years', function(){
-    let exampleAge = new Person ('26');
-    expect (exampleAge.mercAge()).toBe(108);
+describe('Person', function() {
+  it('should return how old a person is in mercurian years', function() {
+    let exampleAge = new Person('26');
+    expect(exampleAge.mercAge()).toBe(108);
 
   });
 });
 
-describe ('Person', function(){
-  it('should return how old a person is in venusian years', function(){
-    let exampleAge = new Person ('26');
-    expect (exampleAge.venusAge()).toBe(41);
+describe('Person', function() {
+  it('should return how old a person is in venusian years', function() {
+    let exampleAge = new Person('26');
+    expect(exampleAge.venusAge()).toBe(41);
 
   });
 });
 
-describe ('Person', function(){
-  it('should return how old a person is in martian years', function(){
-    let exampleAge = new Person ('26');
-    expect (exampleAge.marsAge()).toBe(13);
+describe('Person', function() {
+  it('should return how old a person is in martian years', function() {
+    let exampleAge = new Person('26');
+    expect(exampleAge.marsAge()).toBe(13);
 
   });
 });
 
-describe ('Person', function(){
-  it('should return how old a person is in jovian years', function(){
-    let exampleAge = new Person ('26');
-    expect (exampleAge.jupAge()).toBe(2);
+describe('Person', function() {
+  it('should return how old a person is in jovian years', function() {
+    let exampleAge = new Person('26');
+    expect(exampleAge.jupAge()).toBe(2);
 
   });
 });
 
-describe ('Person', function(){
-  it('calc life expectancy', function(){
-    let lifeLeft = new Person ('26');
-    expect (lifeLeft.mercDeathAge()).toBe(220);
-
+describe('Person', function() {
+  it('calc life expectancy on Mercury', function() {
+    let lifeLeft = new Person('26');
+    expect(lifeLeft.mercDeathAge()).toBe(220);
   });
 });
--- src/scripts.js
+++ src/scripts.js
@@ -1,24 +1,17 @@
 export class Person {
-  constructor(age, mercury, venus, mars, jupiter, continent) {
+  constructor(age) {
     this.age = age;
     this.mercury = this.mercAge();
     this.venus = this.venusAge();
     this.mars = this.marsAge();
     this.jupiter = this.jupAge();
-    this.continent = continent;
     this.lifeExpectancy = 79;
-    this.mercDeathAge = this.mercDeathAge();
+    this.mercDeath = this.mercDeathAge();
   }
 
   checkDigits() {
     let age = parseInt('');
     return this.age;
-  }
-
-  mercDeathAge() {
-    const mercury = .24;
-    let mercurian = parseInt((this.lifeExpectancy - this.age) / mercury);
-    return mercurian;
   }
 
   mercAge() {
@@ -45,5 +38,26 @@ export class Person {
     return jovian;
   }
 
-
+  mercDeathAge() {
+    const mercury = .24;
+    let mercurian = parseInt((this.lifeExpectancy - this.age) / mercury);
+    return mercurian;
+  }
 }
+// venusDeathAge() {
+//   const venus = .62;
+//   let venusian = parseInt(this.age / venus);
+//   return venusian;
+// }
+//
+// marsDeathAge() {
+//   const mars = 1.88;
+//   let martian = parseInt(this.age / mars);
+//   return martian;
+// }
+//
+// jupDeathAge() {
+//   const jupiter = 11.86;
+//   let jovian = parseInt(this.age / jupiter);
+//   return jovian;
+// }
